210cf18fc4f9c69256d9925ccae099a400eff6a0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-07 19:54:21 +0100

    Pass events to parent components using Output decorator

--- ngComponentDatabinding/src/app/app.component.ts
+++ ngComponentDatabinding/src/app/app.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [
-      {
-          type: 'server',
-          name: 'server-title',
-          content: 'server-content'
-      }
-  ];
+    serverElements = [
+        {
+            type: 'server',
+            name: 'server-title',
+            content: 'server-content'
+        }
+    ];
+
+    onServerAdded(serverData: { serverName: string, serverContent: string }) {
+        this.serverElements.push({
+            type: 'server',
+            name: serverData.serverName,
+            content: serverData.serverContent
+        });
+    }
+
+    onBlueprintAdded(blueprintData: { blueprintName: string, blueprintContent: string }) {
+        this.serverElements.push({
+            type: 'blueprint',
+            name: blueprintData.blueprintName,
+            content: blueprintData.blueprintContent
+        });
+    }
+
+
 }
