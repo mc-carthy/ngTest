f06bf7ac98f228b8d31b1d41fb1792b08efa3e1b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-07 19:41:59 +0100

    Alias server-element input variable

--- ngComponentDatabinding/src/app/server-element/server-element.component.ts
+++ ngComponentDatabinding/src/app/server-element/server-element.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-    @Input() element: {
+    @Input('server-element') element: {
         type: string,
         name: string,
         content: string
