31ffad18cd2d6e5ed1515194c212e19a1b850f8b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-10 16:44:34 +0100

    Allow the use of input variables on improved-highlight directive

--- ngDirectives/src/app/shared/directives/improved-highlight/improved-highlight.directive.ts
+++ ngDirectives/src/app/shared/directives/improved-highlight/improved-highlight.directive.ts
@@ -1,24 +1,27 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
     selector: '[appImprovedHighlight]'
 })
 export class ImprovedHighlightDirective implements OnInit {
 
-    @HostBinding('style.backgroundColor') backgroundColor: string;
+    @Input('default-colour') defaultColour: string = 'transparent';
+    @Input('highlight-colour') highlightColour: string = 'blue';
+    @HostBinding('style.backgroundColor') backgroundColour: string;
 
     constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
     ngOnInit() {
+        this.backgroundColour = this.defaultColour;
     }
 
     @HostListener('mouseenter') mouseOver(eventData: Event) {
-        this.backgroundColor = 'blue';
+        this.backgroundColour = this.highlightColour;
         // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
     }
 
     @HostListener('mouseleave') mouseLeave(eventData: Event) {
-        this.backgroundColor = 'transparent';
+        this.backgroundColour = this.defaultColour;
         // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
     }
 
