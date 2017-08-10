a0a699e3bfd4196a3900d28a49e49eaf1adba6f3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-10 16:22:52 +0100

    Use HostListener to create reactive attribute directives

--- ngDirectives/src/app/shared/directives/improved-highlight/improved-highlight.directive.ts
+++ ngDirectives/src/app/shared/directives/improved-highlight/improved-highlight.directive.ts
@@ -1,14 +1,22 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appImprovedHighlight]'
+    selector: '[appImprovedHighlight]'
 })
 export class ImprovedHighlightDirective implements OnInit {
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
-  ngOnInit() {
-      this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-  }
+    constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+    ngOnInit() {
+    }
+
+    @HostListener('mouseenter') mouseOver(eventData: Event) {
+        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    }
+
+    @HostListener('mouseleave') mouseLeave(eventData: Event) {
+        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    }
 
 }
