f01b86cce76c1d5ce39609902c05a2edc529967b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-12 16:38:52 +0100

    Passing query params and configuring how they are handled

--- ngRouter/src/app/servers/edit-server/edit-server.component.ts
+++ ngRouter/src/app/servers/edit-server/edit-server.component.ts
@@ -1,31 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
 @Component({
-  selector: 'app-edit-server',
-  templateUrl: './edit-server.component.html',
-  styleUrls: ['./edit-server.component.css']
+    selector: 'app-edit-server',
+    templateUrl: './edit-server.component.html',
+    styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: { id: number, name: string, status: string };
-  serverName = '';
-  serverStatus = '';
+    server: { id: number, name: string, status: string };
+    serverName = '';
+    serverStatus = '';
+    allowEdit: boolean = false;
 
-  constructor(
-    private serversService: ServersService,
-    private route: ActivatedRoute
-  ) { }
+    constructor(
+        private serversService: ServersService,
+        private route: ActivatedRoute
+    ) { }
 
-  ngOnInit() {
-    this.server = this.serversService.getServer(1);
-    this.serverName = this.server.name;
-    this.serverStatus = this.server.status;
-  }
+    ngOnInit() {
+        this.route.queryParams
+            .subscribe(
+                (queryParams: Params) => {
+                    this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+                }
+            );
+        this.server = this.serversService.getServer(1);
+        this.serverName = this.server.name;
+        this.serverStatus = this.server.status;
+    }
 
-  onUpdateServer() {
-    this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
-  }
+    onUpdateServer() {
+        this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
+    }
 
 }
--- ngRouter/src/app/servers/server/server.component.ts
+++ ngRouter/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 import { ServersService } from '../servers.service';
 
@@ -13,6 +13,7 @@ export class ServerComponent implements OnInit {
 
     constructor(
         private serversService: ServersService,
+        private router: Router,
         private route: ActivatedRoute
     ) { }
 
@@ -26,6 +27,10 @@ export class ServerComponent implements OnInit {
                     this.server = this.serversService.getServer(+params['id']);
                 }
             )
+    }
+
+    onEdit() {
+        this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: 'preserve' });
     }
 
 }
