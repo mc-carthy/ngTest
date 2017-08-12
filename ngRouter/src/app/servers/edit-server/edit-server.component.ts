9b0c821a6ce1df8aec2a7cf22065235a00355e50
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-12 13:26:07 +0100

    Display servers based on route params

--- ngRouter/src/app/servers/edit-server/edit-server.component.ts
+++ ngRouter/src/app/servers/edit-server/edit-server.component.ts
@@ -4,36 +4,28 @@ import { ActivatedRoute } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
-    selector: 'app-edit-server',
-    templateUrl: './edit-server.component.html',
-    styleUrls: ['./edit-server.component.css']
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-    server: { id: number, name: string, status: string };
-    serverName = '';
-    serverStatus = '';
+  server: { id: number, name: string, status: string };
+  serverName = '';
+  serverStatus = '';
 
-    constructor(
-        private serversService: ServersService,
-        private route: ActivatedRoute
-    ) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) { }
 
-    ngOnInit() {
-        console.log("Query params: ");
-        console.log(this.route.snapshot.queryParams);
-        console.log("Fragment: ");
-        console.log(this.route.snapshot.fragment);
+  ngOnInit() {
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
 
-        this.route.queryParams.subscribe();
-        this.route.fragment.subscribe();
-        
-        this.server = this.serversService.getServer(1);
-        this.serverName = this.server.name;
-        this.serverStatus = this.server.status;
-    }
-
-    onUpdateServer() {
-        this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
-    }
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
+  }
 
 }
--- ngRouter/src/app/users/user/user.component.ts
+++ ngRouter/src/app/users/user/user.component.ts
@@ -3,32 +3,33 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from "rxjs/Subscription";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit, OnDestroy {
-  user: {id: number, name: string};
-  paramsSubscription: Subscription;
+    user: { id: number, name: string };
+    paramsSubscription: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    this.user = {
-        id: this.route.snapshot.params['id'],
-        name: this.route.snapshot.params['name']
-    };
-    this.paramsSubscription = this.route.params
-        .subscribe(
+    ngOnInit() {
+        this.user = {
+            id: this.route.snapshot.params['id'],
+            name: this.route.snapshot.params['name']
+        };
+        this.paramsSubscription = this.route.params
+            .subscribe(
             (params: Params) => {
                 this.user.id = params['id'];
                 this.user.name = params['name'];
             }
-        );
-  }
+            );
+    }
 
-  ngOnDestroy() {
-      this.paramsSubscription.unsubscribe();
-  }
+    ngOnDestroy() {
+        this.paramsSubscription.unsubscribe();
+
+    }
 
 }
