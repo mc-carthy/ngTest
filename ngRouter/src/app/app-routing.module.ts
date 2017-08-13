44aaee97d4a79d145621a27a360ef250d62b4e73
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-14 00:06:30 +0100

    Explore useHash option for router module

--- ngRouter/src/app/app-routing.module.ts
+++ ngRouter/src/app/app-routing.module.ts
@@ -30,7 +30,8 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes)
+        // RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes, { useHash: true} )
     ],
     exports: [RouterModule]
 })
