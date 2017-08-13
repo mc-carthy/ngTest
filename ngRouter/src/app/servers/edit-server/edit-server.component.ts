6073a3c9d45e1c1f827761b338d6ff59eda0f5b2
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-13 23:30:30 +0100

    Ensure we are getting the correct server id when editing

--- ngRouter/src/app/servers/edit-server/edit-server.component.ts
+++ ngRouter/src/app/servers/edit-server/edit-server.component.ts
@@ -27,11 +27,17 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
     ngOnInit() {
         this.route.queryParams
             .subscribe(
-            (queryParams: Params) => {
-                this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
-            }
+                (queryParams: Params) => {
+                    this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+                }
             );
-        this.server = this.serversService.getServer(1);
+        const id = this.route.snapshot.params['id'];
+        this.server = this.serversService.getServer(id);
+        this.route.params.subscribe(
+            (params: Params) => {
+                this.server = this.serversService.getServer(params['id']);
+            }
+        );
         this.serverName = this.server.name;
         this.serverStatus = this.server.status;
     }
