6d834eb0474760268bc38a93653d1b6a4dbac141
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-14 19:03:20 +0100

    Unsubscribe from subscriptions

--- ngObservables/src/app/home/home.component.ts
+++ ngObservables/src/app/home/home.component.ts
@@ -1,23 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Observer } from "rxjs/Observer";
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/Rx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    numbersSubscription: Subscription
+    customObservableSubscription: Subscription
 
-  ngOnInit() {
-      const myNumbers = Observable.interval(1000);
-      myNumbers.subscribe(
-          (number: number) => {
-              console.log(number);
-          }
-      );
-  }
+    constructor() { }
+
+    ngOnInit() {
+          const myNumbers = Observable.interval(1000);
+          this.numbersSubscription = myNumbers.subscribe(
+              (number: number) => {
+                  console.log(number);
+              }
+          );
+
+        const myObservable = Observable.create((observer: Observer<string>) => {
+            setTimeout(() => {
+                observer.next('First package');
+            }, 2000);
+            setTimeout(() => {
+                observer.next('Second package');
+            }, 4000);
+            // setTimeout(() => {
+            //     observer.error('This is an error');
+            // }, 5000);
+            setTimeout(() => {
+                observer.complete();
+            }, 5000);
+            // This will not get sent as the observable has already completed
+            setTimeout(() => {
+                observer.next('Third package');
+            }, 6000);
+        });
+
+        this.customObservableSubscription = myObservable.subscribe(
+            (data: string) => {
+                console.log(data);
+            },
+            (error: string) => {
+                console.log(error);
+            },
+            () => {
+                console.log('Completed');
+            }
+        );
+    }
+
+    ngOnDestroy() {
+        this.numbersSubscription.unsubscribe();
+        this.customObservableSubscription.unsubscribe();
+    }
 
 }
