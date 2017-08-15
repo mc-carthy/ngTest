ca49f6439b53e950762a6c956dd4049372988edc
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-15 10:20:41 +0100

    Using ViewChild to submit the form

--- ngTemplateDrivenForms/src/app/app.component.ts
+++ ngTemplateDrivenForms/src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
 
-  onSubmit(form: NgForm) {
-      console.log(form);
-  }
+    @ViewChild('f') signupForm: NgForm;
+
+    suggestUserName() {
+        const suggestedName = 'Superuser';
+    }
+
+    onSubmit() {
+        console.log(this.signupForm);
+    }
+
+    //   onSubmit(form: NgForm) {
+    //       console.log(form);
+    //   }
 }
