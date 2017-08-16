9afb95f1709b30e7562c79f0cae19c78cc7c9ef8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-16 10:32:35 +0100

    Populate forms with default values using property binding

--- ngTemplateDrivenForms/src/app/app.component.ts
+++ ngTemplateDrivenForms/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
 
+    defaultQuestion: string = 'pet';
+
     suggestUserName() {
         const suggestedName = 'Superuser';
     }
