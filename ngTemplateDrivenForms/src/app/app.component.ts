f81754019432ca085e08c6debad68985ca9c5a3a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-16 11:20:22 +0100

    Make use of NgForm.setValue to preset all values in the form

--- ngTemplateDrivenForms/src/app/app.component.ts
+++ ngTemplateDrivenForms/src/app/app.component.ts
@@ -8,15 +8,34 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
 
+    @ViewChild('f') singupForm: NgForm;
     defaultQuestion: string = 'pet';
     answer: string;
     genders = ['male', 'female'];
 
     suggestUserName() {
         const suggestedName = 'Superuser';
+        this.singupForm.setValue({
+            userData: {
+                username: 'Superuser',
+                email: ''
+            },
+            secret: {
+                secretQuestion: 'pet',
+                secretAnswer: '',
+            },
+            gender: 'male'
+        })
     }
 
-      onSubmit(form: NgForm) {
-          console.log(form);
-      }
+    onSuggestUsername() {
+        this.suggestUserName();
+    }
+
+    onSubmit() {
+        console.log(this.singupForm);
+    }
+    //   onSubmit(form: NgForm) {
+    //       console.log(form);
+    //   }
 }
