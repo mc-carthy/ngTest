1373fbc3a4a1dd34225ef28303931ba06e0b867d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-31 16:34:16 +0100

    Wire up reactive form to template

--- ngReactiveForms/src/app/app.component.ts
+++ ngReactiveForms/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, FormControl } from '@angular/forms';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    genders: string[] = ["Male", "Female"];
+    genders: string[] = ["male", "female"];
 
     signupForm: FormGroup;
 
