7fcc9d0326c3b5f284bd5cd4b49dde8f77896516
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-31 17:14:55 +0100

    Create custom input validators

--- ngReactiveForms/src/app/app.component.ts
+++ ngReactiveForms/src/app/app.component.ts
@@ -8,13 +8,13 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
     genders: string[] = ["male", "female"];
-
     signupForm: FormGroup;
+    forbiddenUsernames = ['Mick', 'Test'];
 
     ngOnInit() {
         this.signupForm = new FormGroup({
             'userData': new FormGroup({
-                'username': new FormControl(null, Validators.required),
+                'username': new FormControl(null, [Validators.required, this.isNameForbidden.bind(this)]),
                 'email': new FormControl(null, [Validators.required, Validators.email]),
             }),
             'gender': new FormControl('male'),
@@ -29,5 +29,12 @@ export class AppComponent implements OnInit {
     onAddHobby() {
         const control = new FormControl(null, Validators.required);
         (<FormArray>this.signupForm.get('hobbies')).push(control);
+    }
+
+    isNameForbidden(control: FormControl): {[s: string]: boolean} {
+        if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+            return {'nameIsForbidden': true};
+        }
+        return null;
     }
 }
