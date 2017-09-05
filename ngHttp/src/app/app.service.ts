d2ccc50c0b95c7bc6ec927825ea351e036275819
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-09-05 14:21:28 +0100

    Configuring Http headers

--- ngHttp/src/app/app.service.ts
+++ ngHttp/src/app/app.service.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -7,6 +7,13 @@ export class AppService {
     constructor(private http: Http) { }
 
     storeServers (servers: any[]) {
-        return this.http.post('https://nghttp-ea652.firebaseio.com/data.json', servers);
+        const headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        return this.http.post(
+            'https://nghttp-ea652.firebaseio.com/data.json', 
+            servers, 
+            { headers: headers }
+        );
     }
 }
